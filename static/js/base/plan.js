85f67643641eef28c6fe1ae6d19c9cec865a6a7c
tom <tom@f24ed941-842e-4dd6-a874-434cde5360db>
2013-05-26 20:25:51 +0000

    git-svn-id: http://www.arsbss.be/svn/tfe@12 f24ed941-842e-4dd6-a874-434cde5360db

--- static/js/base/plan.js
+++ static/js/base/plan.js
@@ -184,7 +184,9 @@ Plan.prototype.refresh = function () {
 	this.ctx.clearRect(0,0,this.pl + 1,this.pw + 1);
 	
 	if (/android/.test(navigator.userAgent.toLowerCase())) {
-		ctx.fillStyle = "rgba(255, 255, 255, 0)";
+		alert("android detected");
+		alert(navigator.userAgent.toLowerCase());
+		ctx.fillStyle = "rgba(255, 255, 255, 1)";
 		ctx.fillRect (0, 0, this.pl + 1, this.pw + 1);
 	}
 
