48384ee6b62890fb84c897ffe0c0e9bf449785d4
tom <tom@f24ed941-842e-4dd6-a874-434cde5360db>
2015-01-08 09:36:48 +0000

    Call URL only if latest is different Order by date and id as two readings could arrive the same second
    git-svn-id: http://www.arsbss.be/svn/tfe@104 f24ed941-842e-4dd6-a874-434cde5360db

--- data/models.py
+++ data/models.py
@@ -8,12 +8,12 @@ class ReadingDay(models.Model):
     meter =  models.ForeignKey(Meter,related_name='readingdays')
     date = models.DateField();
     amount = models.FloatField();
-    
+
 class Reading(models.Model):
     meter =  models.ForeignKey(Meter,related_name='readings')
     date = models.DateTimeField();
     amount = models.FloatField();
-    
+
     def get_day(self, day):
         try:
             rd = ReadingDay.objects.get(date=day,meter=self.meter)
@@ -24,21 +24,21 @@ class Reading(models.Model):
             rd.meter = self.meter
             rd.amount = 0
             return rd
-    
+
     def save(self):
         day = datetime.date(self.date.year, self.date.month,self.date.day )
         rd = self.get_day(day)
-        
+
         m = self.meter
         if (m.energy.type == Energy.TYPE_STATE):
             super(Reading, self).save()
             rd.amount = Reading.objects.filter(meter=self.meter,date__gte=datetime.datetime(self.date.year, self.date.month, self.date.day),date__lt=datetime.datetime(self.date.year, self.date.month, self.date.day) + datetime.timedelta(days=1)).aggregate(Avg('amount'))['amount__avg']
-            
+
             if (rd.amount == None):
                 rd.amount = self.amount
             #Removing last value if the 3 last are identical
             try:
-                last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')    
+                last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')
                 if (last.amount == self.amount):
                     beforelast = Reading.objects.filter(meter=self.meter,date__lt=self.date).order_by('-date')[1:2].get()
                     if (beforelast.amount == self.amount):
@@ -49,15 +49,15 @@ class Reading(models.Model):
         else:
             if (m.mode == Meter.MODE_DIFFERENCE):
                 today_consum = Reading.objects.filter(meter=self.meter,date__gte=datetime.datetime(self.date.year, self.date.month, self.date.day),date__lt=datetime.datetime(self.date.year, self.date.month, self.date.day) + datetime.timedelta(days=1)).aggregate(total=Sum('amount'))
-                
+
                 if today_consum['total'] == None:
                     rd.amount = self.amount
                 else:
-                
+
                     rd.amount = today_consum['total'] + self.amount
                     #Here we only remove repeating 0 data
                     try:
-                        last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')    
+                        last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')
                         if (self.amount == 0 and last.amount == self.amount):
                             beforelast = Reading.objects.filter(meter=self.meter,date__lt=self.date).order_by('-date')[1:2].get()
                             if (beforelast.amount == self.amount):
@@ -67,13 +67,13 @@ class Reading(models.Model):
             elif (m.mode == Meter.MODE_INSTANT):
                 try:
                     last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')
-                    
-                    
-                    dt = self.date - last.date 
-                    
-    
+
+
+                    dt = self.date - last.date
+
+
                     rd.amount =   float(rd.amount) + ((float(dt.seconds) * float(self.amount)) / float(3600))
-                    
+
                     #Removing last identical value (if it's not the first)
                     if (last.amount == self.amount):
                         beforelast = Reading.objects.filter(meter=self.meter,date__lt=self.date).order_by('-date')[1:2].get()
@@ -81,44 +81,44 @@ class Reading(models.Model):
                             last.delete()
                 except Reading.DoesNotExist:
                     rd.amount = 0
-                
+
             elif (m.mode == Meter.MODE_TOTAL):
                 try:
                     lastTotal = Reading.objects.filter(meter=self.meter,date__lt=datetime.datetime(self.date.year, self.date.month, self.date.day)).latest('date').amount
-    
-                    rd.amount = (float(self.amount) - float(lastTotal)) 
+
+                    rd.amount = (float(self.amount) - float(lastTotal))
                 except Reading.DoesNotExist:
-                    
+
                     try:
                         lastTotal = Reading.objects.filter(meter=self.meter).order_by('date')[:1].get()
                         rd.amount = (float(self.amount) -  float(lastTotal.amount))
                     except Reading.DoesNotExist:
                         rd.amount = 0
-                    
+
                 #Removing last identical value (if it's not the first)
                 try:
-                    last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')    
+                    last = Reading.objects.filter(meter=self.meter,date__lt=self.date).latest('date')
                     if (last.amount == self.amount):
                         beforelast = Reading.objects.filter(meter=self.meter,date__lt=self.date).order_by('-date')[1:2].get()
                         if (beforelast.amount == self.amount):
                             last.delete()
                 except Reading.DoesNotExist:
-                    pass 
-                
-                                
+                    pass
+
+
             rd.save()
-            
-            
+
+
             super(Reading, self).save()
-            
+
             for a in m.analog_meters.all():
                 a.fire(self.meter.get_instant(86400));
-        
+
     @staticmethod
     def get_instant(meter,delta):
         """Return a list of instant consumption for the given meter during the given time (or since the beginning if no time is given)"""
         if (delta == None):
-            liste = Reading.objects.filter(meter=meter).order_by('date')
+            liste = Reading.objects.filter(meter=meter).order_by('date').order_by('id')
         else:
             liste = Reading.objects.filter(meter=meter,date__lte=datetime.datetime.now(),date__gte=(datetime.datetime.now() - datetime.timedelta(seconds=int(delta)))).order_by('date')
 
@@ -130,18 +130,19 @@ class Reading(models.Model):
         for item in liste:
             count += 1
             temps = (int(calendar.timegm(item.date.timetuple()))* 1000)
+            if (lastTime == temps):
+                temps += 1
             if meter.mode == Meter.MODE_INSTANT:
                 if meter.energy.short_name == 'switch' and float(item.amount)!=last and last >-1:
                     data.append((temps,last))
                 data.append((temps,float(item.amount)))
                 last = float(item.amount)
             elif meter.mode == Meter.MODE_TOTAL:
-                if last > -1 and count > 1 and (temps - lastTime > 0):
+                if last > -1 and count > 1:
                     data.append((temps,((float(item.amount) - last) / (temps - lastTime)) * 3600000))
                 last = float(item.amount)
-                lastTime = temps
             elif meter.mode == Meter.MODE_DIFFERENCE:
                 if (lastTime > -1):
                     data.append((temps,((float(item.amount)) / (temps - lastTime)) * 3600000))
-                lastTime = temps
+            lastTime = temps
         return data
